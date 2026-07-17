e9a2c74422a267149c19d39f493dd5f4f54c2638
Ronan1410 <1410ronan@gmail.com>
2026-07-17 16:26:09 +0530

    assembler can create blocsk of constants, and handle temp command outloc

--- assembler.py
+++ assembler.py
@@ -10,27 +10,56 @@ def run(args):
     for line in assembly.split('\n'):
         if line.strip() == '':
             continue
-        cmd, p1 = line.split()
-        cmd = cmd.lower()
-        op = {
-            'out': '01'
-        }[cmd]
-
-        if p1.endswith('x'):
-            p1 = p1[:-1]
+        split_line = line.split()
+        cmd = split_line[0].lower()
+        p1 = split_line[1] if len(split_line) >= 2 else None
+        if cmd == 'out':
+            # op = {
+            #     'out': '01'
+            # }[cmd]
+            # print('op', op)
+            if p1.endswith('x'):
+                p1 = p1[:-1]
+            else:
+                raise ValueError("param " + p1 + " not recognized")
+            hex_line = '01' + p1
+            hex_lines.append(hex_line)
+        elif cmd == 'outloc':
+            # op = {
+            #     'out': '01'
+            # }[cmd]
+            # print('op', op)
+            if p1.endswith('x'):
+                p1 = p1[:-1]
+            else:
+                raise ValueError("param " + p1 + " not recognized")
+            hex_line = '02' + p1
+            hex_lines.append(hex_line)
+        elif cmd == 'short':
+            assert p1.endswith('x')
+            hex_line = p1[:-1]
+            hex_lines.append(hex_line)
+        elif cmd.endswith(':'):
+            cmd = cmd[:-1]
+            assert cmd.endswith('x')
+            cmd = cmd[:-1]
+            location = int(cmd, 16) // 2
+            while len(hex_lines) < location:
+                hex_lines.append('0000')
         else:
-            raise ValueError("param" + p1 + "not recognized")
-        hex_line = op + p1
-        hex_lines.append(hex_line)
+            raise Exception('cmd ' + cmd + ' not recognised')
     with open(args.out_hex, 'w') as f:
         for hex_line in hex_lines:
             f.write(hex_line + '\n')
+    with open(args.out_hex) as f:
+        for line in f:
+            print(line.strip())
     print('wrote ' + args.out_hex)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--in-asm', type = str, default = 'prog2.asm')
-    parser.add_argument('--out-hex', type = str, default = 'build/prog2.hex')
+    parser.add_argument('--in-asm', type = str, default = 'prog3.asm')
+    parser.add_argument('--out-hex', type = str, default = 'build/prog3.hex')
     args = parser.parse_args()
     run(args)
