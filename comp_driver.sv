7ac675ea98a9b3d20569c37ec8757c67280e12f1
Ronan1410 <1410ronan@gmail.com>
2026-07-25 12:15:48 +0530

    bug fixes for yosys

--- comp_driver.sv
+++ comp_driver.sv
@@ -1,10 +1,11 @@
 module comp_driver();
     reg rst, clk;
     reg [15:0] out;
-    reg[15:0] mem [256];
+    reg [15:0] mem [256];
     reg [3:0] op;
     reg [3:0] reg_select;
     reg [7:0] p1;
+    //reg [7:0] regs[16];
     reg [7:0] x1;
     comp comp1(
         .rst(rst), .clk(clk), .out(out), .mem(mem), .op(op), . p1(p1),
