7d262749dd6f14e37bb8edb44cc0502cd798a262
Ronan1410 <1410ronan@gmail.com>
2026-07-25 12:10:27 +0530

    added mem module

---
+++ mem.sv
@@ -0,0 +1,17 @@
+module mem(
+    input rst, clk,
+    input [15:0] read_addr,
+    input [15:0] write_addr,
+    output [7:0] write_data,
+    input [7:0] write_data
+);
+
+reg [7:0] mem[65536];
+
+always @(posedge clk) begin
+    if (we) begin
+        mem[write_addr] <= write_data;
+    end
+end
+assign read_data = mem[read_addr];
+endmodule
